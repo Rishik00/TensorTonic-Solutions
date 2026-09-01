4246922e3444f775b2c2454b59b6434121f49441
Rishikesh Mallagundla <96735720+Rishik00@users.noreply.github.com>
2026-09-01 16:28:46 +0530

    Solve binomial-pmf-cdf

--- binomial-pmf-cdf/binomial-pmf-cdf.py
+++ binomial-pmf-cdf/binomial-pmf-cdf.py
@@ -1,8 +1,17 @@
 import math
+import numpy as np
 
-def binomial_pmf_cdf(n, p, k):
-    probabilities = [
-        math.comb(n, i) * p ** i * (1.0 - p) ** (n - i)
-        for i in range(k + 1)
-    ]
-    return {"pmf": float(probabilities[k]), "cdf": float(sum(probabilities))}
+def binomial_pmf_cdf(n: int, p: float, k: int) -> dict:
+    """
+    Returns a dictionary with pmf and cdf.
+    """
+    # Write code here
+    n_k = math.factorial(n) / (math.factorial(n-k) * math.factorial(k))
+    term = n_k * math.pow(p, k) * math.pow((1-p), (n-k))
+
+    cdf = 0.0
+    for i in range(k+1):
+        n_i = math.factorial(n) / (math.factorial(n-i) * math.factorial(i))
+        cdf += n_i * math.pow(p, i) * math.pow((1-p), (n-i))
+        
+    return {"pmf": float(term), "cdf": float(cdf)}
