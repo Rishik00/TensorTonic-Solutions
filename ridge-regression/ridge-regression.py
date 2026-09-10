0fb456019f582144087adac5eaefa11fea88346e
Rishikesh Mallagundla <96735720+Rishik00@users.noreply.github.com>
2026-09-10 19:56:45 +0530

    Solve ridge-regression

---
+++ ridge-regression/ridge-regression.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def ridge_regression(X: list, y: list, lam: float) -> list:
+    """
+    Returns the ridge-regression weight vector.
+    """
+    # Write code here
+    X, y = np.asarray(X), np.asarray(y)
+    I = np.eye(X.shape[1])
+    w = np.linalg.inv(np.matmul(X.T, X) + lam * I) @ X.T @ y
+    return w
